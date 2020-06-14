4900d9cc5d0924dd2fb01ebf0ae0b135102821f4
cx-Zh <491907188@qq.com>
2020-06-14 18:23:20 +0800

    finish practice2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,21 @@
-const parseData
+// const parseData
+// export { parseData };
+
+let parseData = function practice1(input){
+    let newcolumn = new Set();
+    for(let item in input.column){
+        let {name: structure, note: n} = input.column[item];
+        newcolumn.add(structure);
+    }
+    console.log(newcolumn);
+    let output = Array.of();
+    for(let item in input.data){
+        let {name, age, gender, birthday} = newcolumn;
+        let [name2, age2, gender2, birthday2] = input.data[item];
+        output.push(({name:name2, age:age2, gender:gender2, birthday:birthday2}))
+    }
+
+    return output
+}
+
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,22 @@
-const inject
+// const inject
+// export { inject };
+
+let inject = function practice2(item, sections){
+    let result = Array.of();
+
+    for(let i in item){
+        if(sections[0]){
+            let{content:section, index:number} = sections[0];
+            if(number == i){
+                result.push(section);
+                sections = sections.slice(1,);
+            }
+        }
+        result.push(item[i]);
+
+    }
+
+    return result;
+}
+
 export { inject };
